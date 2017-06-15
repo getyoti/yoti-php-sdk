19bc78ec3a3263a06e1f35b4d2dc47e4e47b689d
KiranBali <kiran.bali@yoti.com>
2017-06-15 17:16:58 +0100

    Improve example
    * Update Index.php page
    
    Hi Vas,
    As discussed changes made:
    1) added require once autoload
    2) Added commentary
    3) Changed profile attribute method to fixed
    4) Added profile page
    
    Thanks,
    Kiran
    
    * Create index.php
    
    updated although wasnt sure about this:  and replace <?php echo with <?=.
    
    * Create profile.php
    
    * Create profile.php
    
    updated: $parsedQueryString
    
    * Create profile.php
    
    removed white spaces and updated with //uses composer comment

---
+++ example/profile.php
@@ -0,0 +1,32 @@
+<?php
+//require_once __DIR__ . '/../src/boot.php';
+require_once './vendor/autoload.php';//uses composer
+
+$config = [
+    'sdkId' => 'add your SDK ID here',
+    'pemFile' => __DIR__ . '/keys/your-key-name.pem',
+];
+
+$client = new Yoti\YotiClient($config['sdkId'], $config['pemFile']);
+$profile = $client->getActivityDetails($parsedQueryString['token']);
+$attributes = $profile->getProfileAttribute();
+$selfie = base64_encode($attributes['selfie']);
+
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>PROFILE PAGE </title>
+   </head>
+   <body>
+       <?php echo $attributes['given_names']; ?><br>
+       <?php echo $attributes['family_name']; ?><br>
+       <?php echo $attributes['phone_number']; ?><br>
+       <?php echo $attributes['email_address']; ?><br>
+       <?php echo $attributes['date_of_birth']; ?><br>
+       <?php echo $attributes['postal_address']; ?><br>
+       <?php echo $attributes['gender']; ?><br>
+       <?php echo $attributes['nationality']; ?><br>
+       <img src="data:image/x-icon;base64,<?php echo $selfie ?>" /><br>
+   </body>
+ </html>
