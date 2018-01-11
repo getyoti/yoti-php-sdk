53e0d8f393ce28ba06054ae5cee98fe1c6c9d042
Moussa Sidibe <sidibos@users.noreply.github.com>
2018-01-11 16:10:51 +0000

    SDK-166 : Exclude example and tests from published packages. (#14)
    * SDK-166 : Exclude unnecessary files/directories from the PHP package
    
    * SDK-166 : Use attribute getters in the example/profile page

--- example/profile.php
+++ example/profile.php
@@ -1,18 +1,21 @@
 <?php
-require_once './vendor/autoload.php'; // Make sure you run composer update inside the example folder before trying them
+
+// Make sure you run composer update inside the example folder before trying this example out
+require_once './vendor/autoload.php';
+// Log any error message
 $errorMsg = '';
-// Get and sanitize the token
-$token = (isset($_GET['token']))? preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['token']) : '';
+// Get the token
+$token = isset($_GET['token']) ? $_GET['token'] : '';
+
 $config = [
     'sdkId' => 'add your SDK ID here', // This is your SDK ID associated with the Yoti Application you created on Dashboard
-    'pemFile' => __DIR__ . '/keys/your-key-name.pem', // This is the public key (in .pem format) associated with the Yoti Application you created on Dashboard
+    'pemFile' => __DIR__ . '/keys/your-key-name.pem', // This is the private key (in .pem format) associated with the Yoti Application you created on Dashboard
 ];
 
 try {
     $yotiClient = new Yoti\YotiClient($config['sdkId'], $config['pemFile']);
     $profile = $yotiClient->getActivityDetails($token);
-    $attributes = $profile->getProfileAttribute();
-    $selfie = base64_encode($attributes['selfie']);
+    $selfie = base64_encode($profile->getSelfie());
 } catch(\Exception $e) {
     $errorMsg = "Error - {$e->getMessage()}";
 }
@@ -21,20 +24,20 @@ try {
 <html>
    <head>
         <meta charset="utf-8">
-        <title>PROFILE PAGE </title>
+        <title>PROFILE PAGE</title>
    </head>
    <body>
         <?php if (!empty($errorMsg)) : ?>
             <p><strong><?php echo $errorMsg; ?></strong></p>
         <?php else: ?>
-            <strong>Given Name</strong> <?php echo $attributes['given_names']; ?><br>
-            <strong>Family Name</strong> <?php echo $attributes['family_name']; ?><br>
-            <strong>Phone</strong> <?php echo $attributes['phone_number']; ?><br>
-            <strong>Email</strong> <?php echo $attributes['email_address']; ?><br>
-            <strong>Date Of Birth</strong> <?php echo $attributes['date_of_birth']; ?><br>
-            <strong>Address</strong> <?php echo $attributes['postal_address']; ?><br>
-            <strong>Gender</strong> <?php echo $attributes['gender']; ?><br>
-            <strong>Nationality</strong> <?php echo $attributes['nationality']; ?><br>
+            <strong>Given Name(s)</strong> <?php echo $profile->getGivenNames(); ?><br>
+            <strong>Family Name</strong> <?php echo $profile->getFamilyName(); ?><br>
+            <strong>Phone</strong> <?php echo $profile->getPhoneNumber(); ?><br>
+            <strong>Email</strong> <?php echo $profile->getEmailAddress(); ?><br>
+            <strong>Date Of Birth</strong> <?php echo $profile->getDateOfBirth(); ?><br>
+            <strong>Address</strong> <?php echo $profile->getPostalAddress(); ?><br>
+            <strong>Gender</strong> <?php echo $profile->getGender(); ?><br>
+            <strong>Nationality</strong> <?php echo $profile->getNationality(); ?><br>
             <strong>Photo</strong><br> <img src="data:image/x-icon;base64,<?php echo $selfie ?>" />
             <br>
         <?php endif; ?>
