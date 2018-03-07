d8847e151c2737fb6d1f8f98552471f2ba179794
Moussa Sidibe <sidibos@users.noreply.github.com>
2018-03-07 16:02:02 +0000

    SDK-290 : Use SPL class autoloader when not using composer (#23)
    * SDK-290 : Add SPL autoload functionality when not using composer

--- src/boot.php
+++ src/boot.php
@@ -26,32 +26,12 @@ require_once __DIR__ . '/attrpubapi_v1/AttributeList.php';
 require_once __DIR__ . '/attrpubapi_v1/ContentType.php';
 require_once __DIR__ . '/compubapi_v1/EncryptedData.php';
 
-// Load Entities
-require_once __DIR__ . '/Yoti/Entity/Country.php';
-require_once __DIR__ . '/Yoti/Entity/AmlAddress.php';
-require_once __DIR__ . '/Yoti/Entity/AmlProfile.php';
-require_once __DIR__ . '/Yoti/Entity/Selfie.php';
-
-// Load Exceptions
-require_once __DIR__ . '/Yoti/Exception/AmlException.php';
-require_once __DIR__ . '/Yoti/Exception/ActivityDetailsException.php';
-
-// Load main classes
-require_once __DIR__ . '/Yoti/ActivityDetails.php';
-require_once __DIR__ . '/Yoti/YotiClient.php';
-
-// Load Helpers
-require_once __DIR__ . '/Yoti/Helper/ActivityDetailsHelper.php';
-
-// Load Http classes
-require_once __DIR__ . '/Yoti/Http/AbstractRequest.php';
-require_once __DIR__ . '/Yoti/Http/AmlResult.php';
-require_once __DIR__ . '/Yoti/Http/Payload.php';
-require_once __DIR__ . '/Yoti/Http/RestRequest.php';
-require_once __DIR__ . '/Yoti/Http/SignedRequest.php';
-
-// Load Util
-require_once __DIR__ . '/Yoti/Util/Age/AbstractAgeProcessor.php';
-require_once __DIR__ . '/Yoti/Util/Age/AgeUnderOverProcessor.php';
-require_once __DIR__ . '/Yoti/Util/Age/Condition.php';
-require_once __DIR__ . '/Yoti/Util/Age/Processor.php';
+// Autoload Yoti classes
+spl_autoload_register(function($className) {
+    $file = __DIR__ . '/' . str_replace('\\', DIRECTORY_SEPARATOR, $className).'.php';
+    if (file_exists($file)) {
+        require $file;
+        return true;
+    }
+    return false;
+});
