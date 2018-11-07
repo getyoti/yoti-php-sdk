a5708033dbacee92578a1a7bd78e0ebe6a29ab07
Moussa Sidibe <moussa.sidibe@yoti.com>
2018-11-07 15:46:09 +0000

    SDK-658: Mock CurlRequestHandler, Remove Reflection call from the test

--- tests/Http/CurlRequestHandlerTest.php
+++ tests/Http/CurlRequestHandlerTest.php
@@ -3,13 +3,14 @@
 namespace YotiTest\Http;
 
 use Yoti\YotiClient;
-use Yoti\Http\CurlRequestHandler;
-use Yoti\Http\RequestSigner;
 use YotiTest\TestCase;
 use Yoti\Http\Payload;
 use Yoti\Entity\Country;
 use Yoti\Entity\AmlAddress;
 use Yoti\Entity\AmlProfile;
+use Yoti\Http\RequestSigner;
+use Yoti\Http\CurlRequestHandler;
+use Yoti\Http\AbstractRequestHandler;
 
 class CurlRequestHandlerTest extends TestCase
 {
@@ -71,94 +72,28 @@ class CurlRequestHandlerTest extends TestCase
         );
     }
 
-    public function testSendRequestExists()
+    public function testClassIsInstanceOfAbstractRequestHandler()
     {
-        $this->assertTrue(method_exists($this->requestHandler, 'sendRequest'));
+        $this->assertInstanceOf(AbstractRequestHandler::class, $this->requestHandler);
     }
 
-    public function testGenerateRequestHeaders()
+    public function testSendRequest()
     {
-        $signedData = RequestSigner::signRequest(
-            $this->requestHandler,
-            '/aml-check',
-            'POST',
-            $this->payload
+        $expectedResult['response'] = file_get_contents(RECEIPT_JSON);
+        $expectedResult['http_code'] = 200;
+
+        $curlRequestHandler = $this->getMockBuilder('\Yoti\Http\CurlRequestHandler')
+            ->setConstructorArgs([YotiClient::DEFAULT_CONNECT_API, $this->pem, SDK_ID, 'PHP'])
+            ->setMethods(['executeRequest'])
+            ->getMock();
+
+        // Configure the stub.
+        $curlRequestHandler->method('executeRequest')
+            ->willReturn($expectedResult);
+        $result = $curlRequestHandler->sendRequest(
+            '/profile/fakeToken',
+            'GET'
         );
-        $signedMessage = $signedData[RequestSigner::SIGNED_MESSAGE_KEY];
-        $requestHeaders = $this->invokeMethod(
-            $this->requestHandler,
-            'generateRequestHeaders',
-            [$signedMessage]
-        );
-
-        $this->assertEquals(
-            json_encode($this->getHeaders($signedMessage)),
-            json_encode($requestHeaders)
-        );
-    }
-
-    public function getHeaders($signedMessage)
-    {
-        $authKey = $this->getAuthKey();
-
-        $headers = [
-            CurlRequestHandler::YOTI_AUTH_HEADER_KEY . ": {$authKey}",
-            CurlRequestHandler::YOTI_DIGEST_HEADER_KEY . ": {$signedMessage}",
-            CurlRequestHandler::YOTI_SDK_IDENTIFIER_KEY . ": PHP",
-            'Content-Type: application/json',
-            'Accept: application/json',
-        ];
-
-        return $headers;
-    }
-
-    /**
-     * Extract Auth key content to be included in the header.
-     *
-     * @return string
-     */
-    public function getAuthKey()
-    {
-        $pemContent = file_get_contents(PEM_FILE);
-
-        $details = openssl_pkey_get_details(openssl_pkey_get_private($pemContent));
-        if (!array_key_exists('key', $details)) {
-            return NULL;
-        }
-
-        // Remove BEGIN RSA PRIVATE KEY / END RSA PRIVATE KEY lines
-        $key = trim($details['key']);
-        // Support line break on *nix systems, OS, older OS, and Microsoft
-        $_key = preg_split('/\r\n|\r|\n/', $key);
-        if (strpos($key, 'BEGIN') !== FALSE) {
-            array_shift($_key);
-            array_pop($_key);
-        }
-        $key = implode('', $_key);
-
-        // Check auth key is not empty
-        if (empty($key)) {
-            throw new RequestException('Could not retrieve key from PEM.', 401);
-        }
-
-        return $key;
-    }
-
-    /**
-     * Call protected/private method of a class.
-     *
-     * @param object &$object    Instantiated object that we will run method on.
-     * @param string $methodName Method name to call
-     * @param array  $parameters Array of parameters to pass into method.
-     *
-     * @return mixed Method return.
-     */
-    public function invokeMethod(&$object, $methodName, array $parameters = array())
-    {
-        $reflection = new \ReflectionClass(get_class($object));
-        $method = $reflection->getMethod($methodName);
-        $method->setAccessible(true);
-
-        return $method->invokeArgs($object, $parameters);
+        $this->assertEquals(json_encode($expectedResult), json_encode($result));
     }
 }
