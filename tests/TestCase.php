8b2d808e07f8144acbbe09e2341066f4a1d35cd2
David Grayston <davidgrayston@googlemail.com>
2019-02-05 18:02:32 +0000

    SDK-752: Further test coverage for AttributeConverter

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,20 @@ use PHPUnit_Framework_TestCase;
 
 class TestCase extends PHPUnit_Framework_TestCase
 {
-
+    /**
+     * Call protected/private method of a class.
+     *
+     * @param object $class      Class that we will run method on.
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method.
+     *
+     * @return mixed Method return.
+     */
+    public function invokeStaticMethod($class, $methodName, array $parameters = array())
+    {
+        $reflection = new \ReflectionClass($class);
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
+        return $method->invokeArgs(new $class, $parameters);
+    }
 }
