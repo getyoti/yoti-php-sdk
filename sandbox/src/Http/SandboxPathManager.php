d5f21c7ea1991423a8f2cdcfabc1bc1b91a3dd40
David Grayston <davidgrayston@googlemail.com>
2019-04-01 12:45:19 +0100

    SDK-792: Manage sandbox URLs in pipeline

--- sandbox/src/Http/SandboxPathManager.php
+++ sandbox/src/Http/SandboxPathManager.php
@@ -4,22 +4,10 @@ namespace YotiSandbox\Http;
 
 class SandboxPathManager
 {
-    /**
-     * This path is for profile request
-     */
-    const DEFAULT_PROFILE_API_PATH = 'https://dev0.api.yoti.com/sandbox/v1';
-    /**
-     * This path is for creating a Sandbox Application and for a token request
-     */
-    const DEFAULT_TOKEN_API_PATH = 'https://dev0.api.yoti.com:11443/sandbox/v1';
-
     private $tokenApiPath;
     private $profileApiPath;
 
-    public function __construct(
-        $tokenApiPath = self::DEFAULT_TOKEN_API_PATH,
-        $profileApiPath = self::DEFAULT_PROFILE_API_PATH
-    )
+    public function __construct($tokenApiPath, $profileApiPath)
     {
         $this->tokenApiPath = $tokenApiPath;
         $this->profileApiPath = $profileApiPath;
