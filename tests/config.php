8bf7937b4fe0c3268de0857c4ef0f0f1b82caeeb
David Grayston <davidgrayston@googlemail.com>
2019-05-02 14:54:22 +0100

    SDK-934: Improve documentation, messaging and test coverage for pem file/content handling

--- tests/config.php
+++ tests/config.php
@@ -6,6 +6,7 @@ define('SDK_ID', '990a3996-5762-4e8a-aa64-cb406fdb0e68');
 define('RECEIPT_JSON', __DIR__ . '/sample-data/receipt.json');
 define('INVALID_YOTI_CONNECT_TOKEN', 'sdfsdfsdasdajsopifajsd=');
 define('PEM_FILE', __DIR__ . '/sample-data/yw-access-security.pem');
+define('INVALID_PEM_FILE', __DIR__ . '/sample-data/invalid.pem');
 define('DUMMY_SELFIE_FILE', __DIR__ . '/sample-data/dummy-avatar.png');
 define('AML_PRIVATE_KEY', __DIR__ . '/sample-data/aml-check-private-key.pem');
 define('AML_PUBLIC_KEY', __DIR__ . '/sample-data/aml-check-public-key.pem');
