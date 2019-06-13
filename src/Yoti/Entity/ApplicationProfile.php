a8d677d342a39eaaafb2dea2899d28646cfbabd4
David Grayston <davidgrayston@googlemail.com>
2019-06-13 16:34:54 +0100

    SDK-995: Update profile documentation

--- src/Yoti/Entity/ApplicationProfile.php
+++ src/Yoti/Entity/ApplicationProfile.php
@@ -2,6 +2,9 @@
 
 namespace Yoti\Entity;
 
+/**
+ * Profile of an application with convenience methods to access well-known attributes.
+ */
 class ApplicationProfile extends BaseProfile
 {
     const ATTR_APPLICATION_LOGO = 'application_logo';
@@ -9,26 +12,43 @@ class ApplicationProfile extends BaseProfile
     const ATTR_APPLICATION_URL = 'application_url';
     const ATTR_APPLICATION_RECEIPT_BG_COLOR = 'application_receipt_bgcolor';
 
+    /**
+     * The name of the application.
+     *
+     * @return null|Attribute
+     */
     public function getApplicationName()
     {
         return $this->getProfileAttribute(self::ATTR_APPLICATION_NAME);
     }
 
+    /**
+     * The URL where the application is available at.
+     *
+     * @return null|Attribute
+     */
     public function getApplicationUrl()
     {
         return $this->getProfileAttribute(self::ATTR_APPLICATION_URL);
     }
 
     /**
-     * The value will be of type Yoti\Entity\Image
+     * The logo of the application that will be displayed to users that perform a share with it.
      *
      * @return null|Attribute
+     *   The Attribute value will be of type Yoti\Entity\Image
      */
     public function getApplicationLogo()
     {
         return $this->getProfileAttribute(self::ATTR_APPLICATION_LOGO);
     }
 
+    /**
+     * The background colour that will be displayed on each receipt the user gets, as a result
+     * of a share with the application.
+     *
+     * @return null|Attribute
+     */
     public function getApplicationReceiptBgColor()
     {
         return $this->getProfileAttribute(self::ATTR_APPLICATION_RECEIPT_BG_COLOR);
