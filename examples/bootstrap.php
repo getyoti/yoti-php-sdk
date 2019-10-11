8b2b02cc5c082d57df8eb82d2c1dfda74f85c494
David Grayston <davidgrayston@googlemail.com>
2019-10-11 13:59:48 +0100

    SDK-1249: Load variables from .env file only when present

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -5,11 +5,13 @@ require_once __DIR__ .'/vendor/autoload.php';
 
 use Symfony\Component\Dotenv\Dotenv;
 
-// Load environment variables
+// Load environment variables from .env file if present.
 try {
-    $dotenv = new Dotenv();
-    $dotenv->load(__DIR__.'/.env');
-
+    $envFilePath = __DIR__.'/.env';
+    if (is_file($envFilePath)) {
+        $dotenv = new Dotenv();
+        $dotenv->load($envFilePath);
+    }
 } catch(\Exception $e) {
     $errorMessage = "Error loading env variables - {$e->getMessage()}";
     die($errorMessage . PHP_EOL);
