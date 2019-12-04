4389da1e365d6a0e4caa91641e8f4a30e0921bc5
David Grayston <davidgrayston@googlemail.com>
2019-12-04 10:05:33 +0000

    SDK-1304: Test against PHP 7.4

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,3 +6,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/config.php';
+
+/**
+ * Disable deprecated-to-exception conversion in PHP 7.4
+ *
+ * PHPUnit 5.7 is needed whilst we support PHP 5.6, but
+ * uses ReflectionType::__toString(), which will throw
+ * an exception as of PHP 7.4
+ */
+if (version_compare(PHP_VERSION, '7.4.0', '>=')) {
+    PHPUnit_Framework_Error_Deprecated::$enabled = false;
+}
