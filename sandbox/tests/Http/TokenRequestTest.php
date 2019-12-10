72b1c22872c3285a27b1cf171068533033086fc7
David Grayston <davidgrayston@googlemail.com>
2019-12-10 17:57:34 +0000

    SDK-1333: Add test coverage for sandbox request builder and correct date format

--- sandbox/tests/Http/TokenRequestTest.php
+++ sandbox/tests/Http/TokenRequestTest.php
@@ -4,40 +4,54 @@ namespace SandboxTest\Http;
 
 use YotiTest\TestCase;
 use Yoti\Entity\Profile;
+use Yoti\Http\Payload;
 use YotiSandbox\Http\TokenRequest;
 
 class TokenRequestTest extends TestCase
 {
+    const SOME_REMEMBER_ME_ID = 'some_remember_me_id';
+    const SOME_FAMILY_NAME = 'some family name';
+
     /**
      * @var TokenRequest
      */
-    public $tokenRequest;
+    private $tokenRequest;
+
     /**
      * @var array
      */
-    public $sandboxAttrs;
+    private $someSandboxAttributes;
 
     public function setUp()
     {
-        $dummyAttrs = [
-            'name' => Profile::ATTR_FAMILY_NAME,
-            'value' => 'fake_family_name',
-            'derivation' => '',
-            'optional' => 'false',
-            'anchors' => []
+        $this->someSandboxAttributes = [
+            [
+                'name' => Profile::ATTR_FAMILY_NAME,
+                'value' => 'fake_family_name',
+                'derivation' => '',
+                'optional' => 'false',
+                'anchors' => []
+            ]
         ];
-        $this->sandboxAttrs = [
-            $dummyAttrs
-        ];
-        $this->tokenRequest = new TokenRequest('fake_remember_me_id', $this->sandboxAttrs);
+        $this->tokenRequest = new TokenRequest(self::SOME_REMEMBER_ME_ID, $this->someSandboxAttributes);
     }
 
     public function testGetSandboxAttributes()
     {
-        $expectedAttrs = $this->sandboxAttrs;
         $this->assertEquals(
-            json_encode($expectedAttrs),
+            json_encode($this->someSandboxAttributes),
             json_encode($this->tokenRequest->getSandboxAttributes())
+        );
+    }
+
+    public function testGetPayload()
+    {
+        $this->assertEquals(
+            new Payload([
+                'remember_me_id' => self::SOME_REMEMBER_ME_ID,
+                'profile_attributes' => $this->someSandboxAttributes,
+            ]),
+            $this->tokenRequest->getPayload()
         );
     }
 }
