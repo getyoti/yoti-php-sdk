a8060b9e7b18e6ab614355fcb75412872576fbe7
David Grayston <davidgrayston@googlemail.com>
2020-01-13 16:55:51 +0000

    SDK-1313: Remove unneeded class alias

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,12 +5,3 @@
  */
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-/**
- * Allow tests to run with:
- * - PHPUnit 5 for PHP 5.6
- * - PHPUnit 7 for PHP 7
- */
-if (!class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias(\PHPUnit\Framework\TestCase::class, '\PHPUnit_Framework_TestCase');
-}
