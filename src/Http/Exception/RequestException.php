acd0e2a94f9cc9566a7fcde65f128e86fe60024f
David Grayston <davidgrayston@googlemail.com>
2020-01-14 16:19:56 +0000

    SDK-1376: Pass previous exception into custom exceptions

--- src/Http/Exception/RequestException.php
+++ src/Http/Exception/RequestException.php
@@ -5,17 +5,19 @@ namespace Yoti\Http\Exception;
 use Psr\Http\Client\RequestExceptionInterface;
 use Psr\Http\Message\RequestInterface;
 
-class RequestException extends \Exception implements RequestExceptionInterface
+class RequestException extends ClientException implements RequestExceptionInterface
 {
     use RequestAwareTrait;
 
     /**
      * @param string $message
      * @param \Psr\Http\Client\RequestExceptionInterface $request
+     * @param integer $code
+     * @param \Throwable $previous
      */
-    public function __construct($message, RequestInterface $request)
+    public function __construct($message, RequestInterface $request, $code = 0, \Throwable $previous = null)
     {
         $this->setRequest($request);
-        parent::__construct($message);
+        parent::__construct($message, $code, $previous);
     }
 }
