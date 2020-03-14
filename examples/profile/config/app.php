124ef64552c7c98d8ab40ee0d09c6decbae32c18
David Grayston <davidgrayston@googlemail.com>
2020-03-14 02:22:16 +0000

    SDK-1486: Add YotiServiceProvider

--- examples/profile/config/app.php
+++ examples/profile/config/app.php
@@ -169,12 +169,8 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
+        App\Providers\YotiServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
 
     /*
---
+++ examples/profile/config/yoti.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'client.sdk.id' => env('YOTI_SDK_ID'),
+    'pem.file.path' => env('YOTI_KEY_FILE_PATH'),
+];
