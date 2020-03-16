979a497d827d58ec9cd6e4ff1a7300ad02e4f818
David Grayston <davidgrayston@googlemail.com>
2020-03-16 11:53:44 +0000

    SDK-1486: Allow absolute and relative pem file paths

--- examples/profile/config/yoti.php
+++ examples/profile/config/yoti.php
@@ -3,5 +3,7 @@
 return [
     'client.sdk.id' => env('YOTI_SDK_ID'),
     'scenario.id' => env('YOTI_SCENARIO_ID'),
-    'pem.file.path' => env('YOTI_KEY_FILE_PATH'),
+    'pem.file.path' => (function($filePath) {
+        return strpos($filePath, '/') === 0 ? $filePath : base_path($filePath);
+    })(env('YOTI_KEY_FILE_PATH')),
 ];
