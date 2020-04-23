c51ac0b84fb796a0fa386a0044e5a444dfceb98a
David Grayston <davidgrayston@googlemail.com>
2020-04-23 11:40:13 +0100

    SDK-1550: Add type to GeneratedCheckResponse and LivenessResourceResponse

--- tests/DocScan/Session/Retrieve/TaskResponseTest.php
+++ tests/DocScan/Session/Retrieve/TaskResponseTest.php
@@ -6,7 +6,6 @@ namespace Yoti\Test\DocScan\Session\Retrieve;
 
 use Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse;
 use Yoti\DocScan\Session\Retrieve\GeneratedTextDataCheckResponse;
-use Yoti\DocScan\Session\Retrieve\TaskResponse;
 use Yoti\DocScan\Session\Retrieve\TextExtractionTaskResponse;
 use Yoti\Test\TestCase;
 use Yoti\Util\DateTime;
@@ -18,16 +17,42 @@ class TaskResponseTest extends TestCase
 {
     private const SOME_TYPE = 'someType';
     private const SOME_ID = 'someId';
+    private const SOME_OTHER_ID = 'someOtherId';
     private const SOME_STATE = 'someState';
     private const SOME_CREATED = '2019-03-24T03:55:12Z';
     private const SOME_LAST_UPDATED = '2019-03-24T03:55:12Z';
-    private const SOME_GENERATED_CHECKS = [
-        [ 'type' => 'ID_DOCUMENT_TEXT_DATA_CHECK' ],
-    ];
-    private const SOME_GENERATED_MEDIA = [
-        [ 'someKey' => 'someValue' ],
-        [ 'someOtherKey' => 'someOtherValue' ],
-    ];
+    private const SOME_UNKNOWN_TYPE = 'someUnknownType';
+    private const ID_DOCUMENT_TEXT_DATA_CHECK = 'ID_DOCUMENT_TEXT_DATA_CHECK';
+
+    /**
+     * @var TextExtractionTaskResponse
+     */
+    private $taskResponse;
+
+    public function setup(): void
+    {
+         $this->taskResponse = new TextExtractionTaskResponse([
+            'id' => self::SOME_ID,
+            'type' => self::SOME_TYPE,
+            'state' => self::SOME_STATE,
+            'created' => self::SOME_CREATED,
+            'last_updated' => self::SOME_LAST_UPDATED,
+            'generated_checks' => [
+                [
+                    'id' => self::SOME_ID,
+                    'type' => self::ID_DOCUMENT_TEXT_DATA_CHECK,
+                ],
+                [
+                    'id' => self::SOME_OTHER_ID,
+                    'type' => self::SOME_UNKNOWN_TYPE,
+                ],
+            ],
+            'generated_media' => [
+                [],
+                [],
+            ],
+         ]);
+    }
 
     /**
      * @test
@@ -42,67 +67,50 @@ class TaskResponseTest extends TestCase
      * @covers ::getGeneratedMedia
      * @covers ::getGeneratedChecks
      */
-    public function shouldBuildCorrectly()
+    public function shouldMapPropertiesCorrectly()
     {
-        $input = [
-            'id' => self::SOME_ID,
-            'state' => self::SOME_STATE,
-            'created' => self::SOME_CREATED,
-            'last_updated' => self::SOME_LAST_UPDATED,
-            'generated_checks' => self::SOME_GENERATED_CHECKS,
-            'generated_media' => self::SOME_GENERATED_MEDIA,
-            'type' => self::SOME_TYPE,
-        ];
-
-        $result = new TextExtractionTaskResponse($input);
-
-        $this->assertEquals(self::SOME_TYPE, $result->getType());
-        $this->assertEquals(self::SOME_ID, $result->getId());
-        $this->assertEquals(self::SOME_STATE, $result->getState());
-        $this->assertEquals(DateTime::stringToDateTime(self::SOME_CREATED), $result->getCreated());
-        $this->assertEquals(DateTime::stringToDateTime(self::SOME_LAST_UPDATED), $result->getLastUpdated());
-
-        $this->assertCount(1, $result->getGeneratedChecks());
-        $this->assertInstanceOf(GeneratedTextDataCheckResponse::class, $result->getGeneratedChecks()[0]);
-        $this->assertCount(2, $result->getGeneratedMedia());
+        $this->assertEquals(self::SOME_TYPE, $this->taskResponse->getType());
+        $this->assertEquals(self::SOME_ID, $this->taskResponse->getId());
+        $this->assertEquals(self::SOME_STATE, $this->taskResponse->getState());
+        $this->assertEquals(DateTime::stringToDateTime(self::SOME_CREATED), $this->taskResponse->getCreated());
+        $this->assertEquals(DateTime::stringToDateTime(self::SOME_LAST_UPDATED), $this->taskResponse->getLastUpdated());
+        $this->assertCount(2, $this->taskResponse->getGeneratedChecks());
+        $this->assertContainsOnlyInstancesOf(GeneratedCheckResponse::class, $this->taskResponse->getGeneratedChecks());
+        $this->assertCount(2, $this->taskResponse->getGeneratedMedia());
     }
 
     /**
      * @test
      * @covers ::parseGeneratedChecks
+     * @covers ::getGeneratedChecks
      * @covers \Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse::__construct
      * @covers \Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse::getId
+     * @covers \Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse::getType
+     */
+    public function shouldReturnGeneratedTextDataChecks()
+    {
+        $generatedCheck = $this->taskResponse->getGeneratedChecks()[0];
+
+        $this->assertInstanceOf(GeneratedTextDataCheckResponse::class, $generatedCheck);
+        $this->assertEquals(self::SOME_ID, $generatedCheck->getId());
+        $this->assertEquals(self::ID_DOCUMENT_TEXT_DATA_CHECK, $generatedCheck->getType());
+    }
+
+    /**
+     * @test
+     * @covers ::parseGeneratedChecks
+     * @covers ::getGeneratedChecks
+     * @covers \Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse::__construct
+     * @covers \Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse::getId
+     * @covers \Yoti\DocScan\Session\Retrieve\GeneratedCheckResponse::getType
      */
     public function shouldReturnGeneratedCheckWithUnknownType()
     {
-        $input = [
-            'id' => self::SOME_ID,
-            'state' => self::SOME_STATE,
-            'created' => self::SOME_CREATED,
-            'last_updated' => self::SOME_LAST_UPDATED,
-            'generated_checks' => [
-                [
-                    'id' => 'someId',
-                    'type' => 'someUnknownType',
-                ],
-            ],
-            'generated_media' => self::SOME_GENERATED_MEDIA,
-            'type' => self::SOME_TYPE,
-        ];
+        $generatedCheck = $this->taskResponse->getGeneratedChecks()[1];
 
-        $result = new TextExtractionTaskResponse($input);
-
-        $this->assertEquals(self::SOME_TYPE, $result->getType());
-        $this->assertEquals(self::SOME_ID, $result->getId());
-        $this->assertEquals(self::SOME_STATE, $result->getState());
-        $this->assertEquals(DateTime::stringToDateTime(self::SOME_CREATED), $result->getCreated());
-        $this->assertEquals(DateTime::stringToDateTime(self::SOME_LAST_UPDATED), $result->getLastUpdated());
-
-        $this->assertCount(2, $result->getGeneratedMedia());
-
-        $this->assertCount(1, $result->getGeneratedChecks());
-        $this->assertEquals('someId', $result->getGeneratedChecks()[0]->getId());
-        $this->assertInstanceOf(GeneratedCheckResponse::class, $result->getGeneratedChecks()[0]);
+        $this->assertInstanceOf(GeneratedCheckResponse::class, $generatedCheck);
+        $this->assertEquals(self::SOME_OTHER_ID, $generatedCheck->getId());
+        $this->assertEquals(self::SOME_UNKNOWN_TYPE, $generatedCheck->getType());
     }
 
     /**
@@ -112,27 +120,8 @@ class TaskResponseTest extends TestCase
      */
     public function shouldFilterGeneratedTextDataChecks(): void
     {
-        $input = [
-            'id' => self::SOME_ID,
-            'state' => self::SOME_STATE,
-            'created' => self::SOME_CREATED,
-            'last_updated' => self::SOME_LAST_UPDATED,
-            'generated_checks' => [
-                [
-                    'type' => 'someUnknownType',
-                ],
-                [
-                    'type' => 'ID_DOCUMENT_TEXT_DATA_CHECK',
-                ]
-            ],
-            'generated_media' => self::SOME_GENERATED_MEDIA,
-            'type' => self::SOME_TYPE,
-        ];
-
-        $result = new TaskResponse($input);
-
-        $this->assertCount(2, $result->getGeneratedChecks());
-        $this->assertCount(1, $result->getGeneratedTextDataChecks());
+        $this->assertCount(2, $this->taskResponse->getGeneratedChecks());
+        $this->assertCount(1, $this->taskResponse->getGeneratedTextDataChecks());
     }
 
     /**
