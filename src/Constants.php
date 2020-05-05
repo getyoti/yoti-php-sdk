f19da4a8ad602fe714c0e5720ce401a4fb131065
David Grayston <davidgrayston@googlemail.com>
2020-05-05 18:26:43 +0100

    Bump minor version

--- src/Constants.php
+++ src/Constants.php
@@ -19,7 +19,7 @@ class Constants
     public const SDK_IDENTIFIER = 'PHP';
 
     /** Default SDK version */
-    public const SDK_VERSION = '3.1.0';
+    public const SDK_VERSION = '3.2.0';
 
     /** Base url for connect page (user will be redirected to this page eg. baseurl/app-id) */
     public const CONNECT_BASE_URL = 'https://www.yoti.com/connect';
