d28c7a6fecd6bc3c0525a7a1a9dd41ea08da90e7
David Grayston <davidgrayston@googlemail.com>
2020-08-29 21:13:03 +0100

    SDK-1727: Return default HTTP client and logger if not set

--- src/DocScan/DocScanClient.php
+++ src/DocScan/DocScanClient.php
@@ -9,11 +9,9 @@ use Yoti\DocScan\Session\Create\CreateSessionResult;
 use Yoti\DocScan\Session\Create\SessionSpecification;
 use Yoti\DocScan\Session\Retrieve\GetSessionResult;
 use Yoti\DocScan\Support\SupportedDocumentsResponse;
-use Yoti\Http\Client;
 use Yoti\Media\Media;
 use Yoti\Util\Config;
 use Yoti\Util\Env;
-use Yoti\Util\Logger;
 use Yoti\Util\PemFile;
 use Yoti\Util\Validation;
 
@@ -53,12 +51,6 @@ class DocScanClient
     ) {
         Validation::notEmptyString($sdkId, 'SDK ID');
         $pemFile = PemFile::resolveFromString($pem);
-
-        // Create default HTTP client.
-        $options[Config::HTTP_CLIENT] = $options[Config::HTTP_CLIENT] ?? new Client();
-
-        // Create default logger.
-        $options[Config::LOGGER] = $options[Config::LOGGER] ?? new Logger();
 
         // Set API URL from environment variable.
         $options[Config::API_URL] = $options[Config::API_URL] ?? Env::get(Constants::ENV_DOC_SCAN_API_URL);
