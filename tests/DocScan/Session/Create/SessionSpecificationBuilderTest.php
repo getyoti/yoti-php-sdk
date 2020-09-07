ba7dd6fca218a1756b11efbba92b72d5e1930831
David Grayston <davidgrayston@googlemail.com>
2020-09-07 19:53:49 +0100

    SDK-1744: Add support for biometric consent

--- tests/DocScan/Session/Create/SessionSpecificationBuilderTest.php
+++ tests/DocScan/Session/Create/SessionSpecificationBuilderTest.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Yoti\Test\DocScan\Session\Create;
 
-use Yoti\DocScan\Session\Create\Check\RequestedDocumentAuthenticityCheck;
-use Yoti\DocScan\Session\Create\Check\RequestedLivenessCheck;
+use Yoti\DocScan\Session\Create\Check\RequestedCheck;
 use Yoti\DocScan\Session\Create\Filters\RequiredDocument;
 use Yoti\DocScan\Session\Create\NotificationConfig;
 use Yoti\DocScan\Session\Create\SdkConfig;
 use Yoti\DocScan\Session\Create\SessionSpecificationBuilder;
-use Yoti\DocScan\Session\Create\Task\RequestedTextExtractionTask;
+use Yoti\DocScan\Session\Create\Task\RequestedTask;
 use Yoti\Test\TestCase;
 
 /**
@@ -18,10 +17,52 @@ use Yoti\Test\TestCase;
  */
 class SessionSpecificationBuilderTest extends TestCase
 {
-
     private const SOME_CLIENT_SESSION_TOKEN_TTL = 30;
     private const SOME_RESOURCES_TTL = 65000;
     private const SOME_USER_TRACKING_ID = 'someUserTrackingId';
+
+    /**
+     * @var SdkConfig
+     */
+    private $sdkConfigMock;
+
+    /**
+     * @var NotificationConfig
+     */
+    private $notificationsMock;
+
+    /**
+     * @var RequestedCheck
+     */
+    private $requestedCheckMock;
+
+    /**
+     * @var RequestedTask
+     */
+    private $requestedTaskMock;
+
+    /**
+     * @var RequiredDocument
+     */
+    private $requiredDocumentMock;
+
+    public function setup(): void
+    {
+        $this->sdkConfigMock = $this->createMock(SdkConfig::class);
+        $this->sdkConfigMock->method('jsonSerialize')->willReturn(['sdkConfig']);
+
+        $this->notificationsMock = $this->createMock(NotificationConfig::class);
+        $this->notificationsMock->method('jsonSerialize')->willReturn(['notifications']);
+
+        $this->requestedCheckMock = $this->createMock(RequestedCheck::class);
+        $this->requestedCheckMock->method('jsonSerialize')->willReturn(['requestedChecks']);
+
+        $this->requestedTaskMock = $this->createMock(RequestedTask::class);
+        $this->requestedTaskMock->method('jsonSerialize')->willReturn(['requestedTasks']);
+
+        $this->requiredDocumentMock = $this->createMock(RequiredDocument::class);
+        $this->requiredDocumentMock->method('jsonSerialize')->willReturn((object) ['requiredDocument']);
+    }
 
     /**
      * @test
@@ -34,6 +75,7 @@ class SessionSpecificationBuilderTest extends TestCase
      * @covers ::withRequestedTask
      * @covers ::withSdkConfig
      * @covers ::withRequiredDocument
+     * @covers ::withBlockBiometricConsent
      * @covers \Yoti\DocScan\Session\Create\SessionSpecification::__construct
      * @covers \Yoti\DocScan\Session\Create\SessionSpecification::getClientSessionTokenTtl
      * @covers \Yoti\DocScan\Session\Create\SessionSpecification::getResourcesTtl
@@ -43,41 +85,39 @@ class SessionSpecificationBuilderTest extends TestCase
      * @covers \Yoti\DocScan\Session\Create\SessionSpecification::getRequestedTasks
      * @covers \Yoti\DocScan\Session\Create\SessionSpecification::getSdkConfig
      * @covers \Yoti\DocScan\Session\Create\SessionSpecification::getRequiredDocuments
+     * @covers \Yoti\DocScan\Session\Create\SessionSpecification::getBlockBiometricConsent
      */
     public function shouldCorrectlyBuildSessionSpecification()
     {
-        $sdkConfigMock = $this->createMock(SdkConfig::class);
-        $notificationsMock = $this->createMock(NotificationConfig::class);
-        $requestedCheckMock = $this->createMock(RequestedDocumentAuthenticityCheck::class);
-        $requestedTaskMock = $this->createMock(RequestedTextExtractionTask::class);
-        $requiredDocumentMock = $this->createMock(RequiredDocument::class);
-
         $sessionSpecification = (new SessionSpecificationBuilder())
             ->withClientSessionTokenTtl(self::SOME_CLIENT_SESSION_TOKEN_TTL)
             ->withResourcesTtl(self::SOME_RESOURCES_TTL)
-            ->withNotifications($notificationsMock)
+            ->withNotifications($this->notificationsMock)
             ->withUserTrackingId(self::SOME_USER_TRACKING_ID)
-            ->withRequestedCheck($requestedCheckMock)
-            ->withRequestedTask($requestedTaskMock)
-            ->withSdkConfig($sdkConfigMock)
-            ->withRequiredDocument($requiredDocumentMock)
+            ->withRequestedCheck($this->requestedCheckMock)
+            ->withRequestedTask($this->requestedTaskMock)
+            ->withSdkConfig($this->sdkConfigMock)
+            ->withRequiredDocument($this->requiredDocumentMock)
+            ->withBlockBiometricConsent(true)
             ->build();
 
         $this->assertEquals(self::SOME_CLIENT_SESSION_TOKEN_TTL, $sessionSpecification->getClientSessionTokenTtl());
         $this->assertEquals(self::SOME_RESOURCES_TTL, $sessionSpecification->getResourcesTtl());
         $this->assertEquals(self::SOME_USER_TRACKING_ID, $sessionSpecification->getUserTrackingId());
-        $this->assertEquals($notificationsMock, $sessionSpecification->getNotifications());
+        $this->assertEquals($this->notificationsMock, $sessionSpecification->getNotifications());
 
         $this->assertCount(1, $sessionSpecification->getRequestedChecks());
-        $this->assertEquals($requestedCheckMock, $sessionSpecification->getRequestedChecks()[0]);
+        $this->assertEquals($this->requestedCheckMock, $sessionSpecification->getRequestedChecks()[0]);
 
         $this->assertCount(1, $sessionSpecification->getRequestedTasks());
-        $this->assertEquals($requestedTaskMock, $sessionSpecification->getRequestedTasks()[0]);
+        $this->assertEquals($this->requestedTaskMock, $sessionSpecification->getRequestedTasks()[0]);
 
-        $this->assertEquals($sdkConfigMock, $sessionSpecification->getSdkConfig());
+        $this->assertEquals($this->sdkConfigMock, $sessionSpecification->getSdkConfig());
 
         $this->assertCount(1, $sessionSpecification->getRequiredDocuments());
-        $this->assertEquals($requiredDocumentMock, $sessionSpecification->getRequiredDocuments()[0]);
+        $this->assertEquals($this->requiredDocumentMock, $sessionSpecification->getRequiredDocuments()[0]);
+
+        $this->assertTrue($sessionSpecification->getBlockBiometricConsent());
     }
 
     /**
@@ -87,22 +127,18 @@ class SessionSpecificationBuilderTest extends TestCase
      */
     public function shouldOverwriteCurrentListWithRequestedChecks()
     {
-        $firstRequestedCheckMock = $this->createMock(RequestedDocumentAuthenticityCheck::class);
-        $secondRequestedCheckMock = $this->createMock(RequestedLivenessCheck::class);
-
-        $checkList = [ $secondRequestedCheckMock ];
+        $someOtherRequestedCheckMock = $this->createMock(RequestedCheck::class);
 
         $sessionSpecification = (new SessionSpecificationBuilder())
             ->withClientSessionTokenTtl(self::SOME_CLIENT_SESSION_TOKEN_TTL)
             ->withResourcesTtl(self::SOME_RESOURCES_TTL)
             ->withUserTrackingId(self::SOME_USER_TRACKING_ID)
-            ->withRequestedCheck($firstRequestedCheckMock)
-            ->withRequestedChecks($checkList)
+            ->withRequestedCheck($this->requestedCheckMock)
+            ->withRequestedChecks([ $someOtherRequestedCheckMock ])
             ->build();
 
         $this->assertCount(1, $sessionSpecification->getRequestedChecks());
-        $this->assertInstanceOf(RequestedLivenessCheck::class, $sessionSpecification->getRequestedChecks()[0]);
-        $this->assertEquals($secondRequestedCheckMock, $sessionSpecification->getRequestedChecks()[0]);
+        $this->assertSame($someOtherRequestedCheckMock, $sessionSpecification->getRequestedChecks()[0]);
     }
 
     /**
@@ -112,21 +148,18 @@ class SessionSpecificationBuilderTest extends TestCase
      */
     public function shouldOverwriteCurrentListWithRequestedTasks()
     {
-        $firstRequestedTaskMock = $this->createMock(RequestedTextExtractionTask::class);
-        $secondRequestedTaskMock = $this->createMock(RequestedTextExtractionTask::class);
-
-        $checkList = [ $secondRequestedTaskMock ];
+        $someOtherRequestedTaskMock = $this->createMock(RequestedTask::class);
 
         $sessionSpecification = (new SessionSpecificationBuilder())
             ->withClientSessionTokenTtl(self::SOME_CLIENT_SESSION_TOKEN_TTL)
             ->withResourcesTtl(self::SOME_RESOURCES_TTL)
             ->withUserTrackingId(self::SOME_USER_TRACKING_ID)
-            ->withRequestedTask($firstRequestedTaskMock)
-            ->withRequestedTasks($checkList)
+            ->withRequestedTask($this->requestedTaskMock)
+            ->withRequestedTasks([ $someOtherRequestedTaskMock ])
             ->build();
 
         $this->assertCount(1, $sessionSpecification->getRequestedTasks());
-        $this->assertEquals($secondRequestedTaskMock, $sessionSpecification->getRequestedTasks()[0]);
+        $this->assertSame($someOtherRequestedTaskMock, $sessionSpecification->getRequestedTasks()[0]);
     }
 
     /**
@@ -135,43 +168,91 @@ class SessionSpecificationBuilderTest extends TestCase
      */
     public function shouldReturnCorrectJsonString()
     {
-        $sdkConfigMock = $this->createMock(SdkConfig::class);
-        $sdkConfigMock->method('jsonSerialize')->willReturn(['sdkConfig']);
-
-        $notificationsMock = $this->createMock(NotificationConfig::class);
-        $notificationsMock->method('jsonSerialize')->willReturn(['notifications']);
-
-        $requestedCheckMock = $this->createMock(RequestedDocumentAuthenticityCheck::class);
-        $requestedCheckMock->method('jsonSerialize')->willReturn(['requestedChecks']);
-
-        $requestedTaskMock = $this->createMock(RequestedTextExtractionTask::class);
-        $requestedTaskMock->method('jsonSerialize')->willReturn(['requestedTasks']);
-
-        $requiredDocumentMock = $this->createMock(RequiredDocument::class);
-        $requiredDocumentMock->method('jsonSerialize')->willReturn((object) ['requiredDocument']);
-
         $sessionSpecification = (new SessionSpecificationBuilder())
             ->withClientSessionTokenTtl(self::SOME_CLIENT_SESSION_TOKEN_TTL)
             ->withResourcesTtl(self::SOME_RESOURCES_TTL)
-            ->withNotifications($notificationsMock)
+            ->withNotifications($this->notificationsMock)
             ->withUserTrackingId(self::SOME_USER_TRACKING_ID)
-            ->withRequestedCheck($requestedCheckMock)
-            ->withRequestedTask($requestedTaskMock)
-            ->withSdkConfig($sdkConfigMock)
-            ->withRequiredDocument($requiredDocumentMock)
+            ->withRequestedCheck($this->requestedCheckMock)
+            ->withRequestedTask($this->requestedTaskMock)
+            ->withSdkConfig($this->sdkConfigMock)
+            ->withRequiredDocument($this->requiredDocumentMock)
+            ->withBlockBiometricConsent(true)
             ->build();
 
-        $expected = [
-            'client_session_token_ttl' => self::SOME_CLIENT_SESSION_TOKEN_TTL,
-            'resources_ttl' => self::SOME_RESOURCES_TTL,
-            'user_tracking_id' => self::SOME_USER_TRACKING_ID,
-            'notifications' => $notificationsMock,
-            'sdk_config' => $sdkConfigMock,
-            'requested_checks' => [ $requestedCheckMock ],
-            'requested_tasks' => [ $requestedTaskMock ],
-            'required_documents' => [ $requiredDocumentMock ],
-        ];
+        $this->assertJsonStringEqualsJsonString(
+            json_encode([
+                'client_session_token_ttl' => self::SOME_CLIENT_SESSION_TOKEN_TTL,
+                'resources_ttl' => self::SOME_RESOURCES_TTL,
+                'user_tracking_id' => self::SOME_USER_TRACKING_ID,
+                'notifications' => $this->notificationsMock,
+                'sdk_config' => $this->sdkConfigMock,
+                'requested_checks' => [ $this->requestedCheckMock ],
+                'requested_tasks' => [ $this->requestedTaskMock ],
+                'required_documents' => [ $this->requiredDocumentMock ],
+                'block_biometric_consent' => true,
+            ]),
+            json_encode($sessionSpecification)
+        );
+    }
 
-        $this->assertJsonStringEqualsJsonString(json_encode($expected), json_encode($sessionSpecification));
+    /**
+     * @test
+     * @covers \Yoti\DocScan\Session\Create\SessionSpecification::jsonSerialize
+     */
+    public function shouldReturnCorrectJsonStringWithoutOptionalProperties()
+    {
+        $sessionSpecification = (new SessionSpecificationBuilder())->build();
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode([
+                'requested_checks' => [],
+                'requested_tasks' => [],
+                'required_documents' => [],
+            ]),
+            json_encode($sessionSpecification)
+        );
+    }
+
+    /**
+     * @test
+     * @covers \Yoti\DocScan\Session\Create\SessionSpecification::jsonSerialize
+     */
+    public function shouldReturnCorrectJsonStringWithBlockBiometricConsentTrue()
+    {
+        $sessionSpecification = (new SessionSpecificationBuilder())
+            ->withBlockBiometricConsent(true)
+            ->build();
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode([
+                'requested_checks' => [],
+                'requested_tasks' => [],
+                'required_documents' => [],
+                'block_biometric_consent' => true,
+            ]),
+            json_encode($sessionSpecification)
+        );
+    }
+
+    /**
+     * @test
+     * @covers \Yoti\DocScan\Session\Create\SessionSpecification::jsonSerialize
+     */
+    public function shouldReturnCorrectJsonStringWithBlockBiometricConsentFalse()
+    {
+        $sessionSpecification = (new SessionSpecificationBuilder())
+            ->withBlockBiometricConsent(false)
+            ->build();
+
+        $this->assertJsonStringEqualsJsonString(
+            json_encode([
+                'requested_checks' => [],
+                'requested_tasks' => [],
+                'required_documents' => [],
+                'block_biometric_consent' => false,
+            ]),
+            json_encode($sessionSpecification)
+        );
     }
 }
