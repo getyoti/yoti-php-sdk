f41c1ad0ff366c0ac2e1d0cc0018e5ff19d88a2d
David Grayston <davidgrayston@googlemail.com>
2020-09-08 18:18:09 +0100

    SDK-1747: Remove Face Map from demo

--- examples/doc-scan/app/Http/Controllers/MediaController.php
+++ examples/doc-scan/app/Http/Controllers/MediaController.php
@@ -15,11 +15,6 @@ class MediaController extends BaseController
         $content = $media->getContent();
         $contentType = $media->getMimeType();
 
-        if ($request->get('base64') === '1' && $contentType === 'application/octet-stream') {
-            $content = base64_decode($content, true);
-            $contentType = (new \finfo(FILEINFO_MIME))->buffer($content);
-        }
-
         return response($content, 200)->header('Content-Type', $contentType);
     }
 }
