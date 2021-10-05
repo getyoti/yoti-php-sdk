bb41179248b40ddf468cca77e63183ff8e1659a4
Rodion Liuborets <rodion.liuborets@yoti.com>
2021-10-05 18:20:51 +0300

    SDK-1913 Small fix for WatchlistCheckResponse

--- src/DocScan/Session/Retrieve/Contracts/WatchlistCheckResponse.php
+++ src/DocScan/Session/Retrieve/Contracts/WatchlistCheckResponse.php
@@ -4,20 +4,7 @@ declare(strict_types=1);
 
 namespace Yoti\DocScan\Session\Retrieve\Contracts;
 
-use Yoti\DocScan\Session\Retrieve\ReportResponse;
-
 abstract class WatchlistCheckResponse extends ProfileCheckResponse
 {
-    /**
-     * @var null|ReportResponse
-     */
-    private $report;
 
-    /**
-     * @return ReportResponse|null
-     */
-    public function getReport(): ?ReportResponse
-    {
-        return $this->report;
-    }
 }
