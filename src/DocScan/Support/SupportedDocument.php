0a1ebc96936d1d47206c1fb290538eee0595df86
Rodion Liuborets <rodion.liuborets@yoti.com>
2021-12-24 12:36:03 +0200

    SDK-2042 Add supporting for fetching supported documents

--- src/DocScan/Support/SupportedDocument.php
+++ src/DocScan/Support/SupportedDocument.php
@@ -12,11 +12,17 @@ class SupportedDocument
     private $type;
 
     /**
+     * @var bool|null
+     */
+    private $isStrictlyLatin;
+
+    /**
      * @param array<string, mixed> $document
      */
     public function __construct(array $document)
     {
         $this->type = $document['type'] ?? null;
+        $this->isStrictlyLatin = $document['is_strictly_latin'] ?? null;
     }
 
     /**
@@ -25,5 +31,13 @@ class SupportedDocument
     public function getType(): ?string
     {
         return $this->type;
+    }
+
+    /**
+     * @return bool|null
+     */
+    public function getIsStrictlyLatin(): ?bool
+    {
+        return $this->isStrictlyLatin;
     }
 }
