f5cc6d9c9a2854003296863261f104520e9fad2b
mehmet-yoti <mehmet.sepici@yoti.com>
2024-02-02 00:38:28 +0000

    www19

--- src/Profile/Service.php
+++ src/Profile/Service.php
@@ -88,7 +88,7 @@ class Service
 
         $this->checkForReceipt($result);
         $receipt = new Receipt($result['receipt'], $this->config->getLogger());
-        error_log("LOG 10=>" . json_encode($result['receipt']));
+        //error_log("LOG 10=>" . json_encode($result['receipt']));
 
        // error_log("LOG 11 =>" . json_encode($result['receipt']['other_party_profile_content']));
       //  error_log("LOG 12" . $receipt.parseOtherPartyProfileContent());
